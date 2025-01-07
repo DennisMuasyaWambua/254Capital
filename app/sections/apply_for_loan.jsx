0cb3e19f9363426e69ffc34a9a681a10bcb2eda6
Dennis wambua <muasyathegreat4@gmail.com>
2025-01-07 15:26:57 +0300

    railway urls restored

--- app/sections/apply_for_loan.jsx
+++ app/sections/apply_for_loan.jsx
@@ -39,7 +39,7 @@ const ApplyForLoan = () => {
     setResponseMessage("");
 
     try {
-      const FORM_ENDPOINT = "https://api.254-capital.com/mail/loan-applications/";
+      const FORM_ENDPOINT = "https://254capitalemailserver-production.up.railway.app/mail/loan-applications/";
 
       if (!FORM_ENDPOINT) {
         throw new Error("Form endpoint is not set.");
